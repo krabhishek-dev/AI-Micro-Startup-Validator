1f62dbc3b684b9bc39bbee62562757808b71c268
Abhishek Kumar <abhibth977@gmail.com>
2026-03-27 20:38:41 +0530

    updating app.jsx

--- Frontend_Server/src/App.jsx
+++ Frontend_Server/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './pages/home.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import SubmitIdea from "./pages/SubmitIdea";
+// import Result from "./pages/Result";
+// import Dashboard from "./pages/Dashboard";
+// import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    // <Router>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/submit" element={<SubmitIdea />} />
+    //     <Route path="/result" element={<Result />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //   </Routes>
+    // </Router>
+
+    <div>
+      <h2>
+        This page is under maintanence....
+      </h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend_Server/src/services/api.js
@@ -0,0 +1,5 @@
+// import axios from "axios";
+
+// export const analyzeIdea = async (data) => {
+//   return await axios.post("http://localhost:5000/api/analyze", data);
+// };
